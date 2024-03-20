9b4813c91854319af42dee3dc457f287d5099040
connertm24 <connermckeone42@gmail.com>
2024-03-20 10:28:37 -0700

    skill stuff

--- DnD/src/Proficencies.java
+++ DnD/src/Proficencies.java
@@ -1,2 +1,19 @@
 public class Proficencies {
+
+    private static final Proficencies instance = new Proficencies();
+
+    // private constructor to avoid client applications using the constructor
+    private Proficencies(){}
+
+    public static Proficencies getInstance() {
+        return instance;
+    }
+
+    public void addskill(){
+
+    }
+
+    public int getskill(String id){
+        return 0;
+    }
 }
