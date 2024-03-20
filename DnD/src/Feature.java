9908f0b9215c36bc1d52c14a50bb765eaef3ddcc
connertm24 <connermckeone42@gmail.com>
2024-03-20 09:39:59 -0700

    Feature.java

---
+++ DnD/src/Feature.java
@@ -0,0 +1,13 @@
+public class Feature {
+    public String name;
+    public String text;
+    public String type;
+    public String origin;
+    public Feature(String Nname, String body, String place, String source){
+        name = Nname;
+        text = body;
+        type = place;
+        origin = source;
+
+    }
+}
