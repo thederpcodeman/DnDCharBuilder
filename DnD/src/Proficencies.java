4d14b92bb1fb549823ce280672138d0fab6f755d
connertm24 <connermckeone42@gmail.com>
2024-03-21 08:43:08 -0700

    Proficiency system

--- DnD/src/Proficencies.java
+++ DnD/src/Proficencies.java
@@ -1,28 +1,34 @@
+import java.util.ArrayList;
+
 public class Proficencies {
-    public int Atokens;
-
-    public int Wtokens;
-
-    public int Stokens;
-
-    public int Ttokens;
+    public ArrayList<Skill> list;
 
 
-
-    private static final Proficencies instance = new Proficencies();
-
-    // private constructor to avoid client applications using the constructor
-    private Proficencies(){}
-
-    public static Proficencies getInstance() {
-        return instance;
+    public Proficencies() {
+        list = new ArrayList<>();
     }
 
-    public void addskill(){
-
+    public void addskill(Skill n){
+        if (n.level <= getskill(n.name.toLowerCase())){
+                return;
+        }
+        for (Skill i : list){
+            if (i.name.equalsIgnoreCase(n.name)){
+                i.level = n.level;
+                return;
+            }
+        }
+        list.add(n);
+        return;
     }
 
     public int getskill(String id){
+        for (Skill i : list){
+            if (i.name.equalsIgnoreCase(id)){
+                return (i.level);
+            }
+        }
         return 0;
     }
+
 }
--- DnD/src/Skill.java
+++ DnD/src/Skill.java
@@ -1,2 +1,12 @@
 public class Skill {
+    public String name;
+    public int level; // 0:not proficient, 1:Proficient, 2:Expertise
+    public int mod; // 1:Str, 2:Dex, 3:Con, 4:Int, 5:Wis, 6:Cha, Anything_else:None
+
+    public String type; // A, mW, sW, Sk, T, L
+    public Skill(String call, int lv, int m){
+        name = call.toLowerCase();
+        level = lv;
+        mod = m;
+    }
 }
