ab9f7c51f6ae2e724a97ce16d554081cdb31ca5b
connertm24 <connermckeone42@gmail.com>
2024-03-21 09:04:47 -0700

    Proficiency system applied to elves

--- DnD/src/Main.java
+++ DnD/src/Main.java
@@ -443,7 +443,6 @@ public class Main {
                         }else if (uInint == 2){
                             Str += 2;
                             subrace = "Mountain Dwarf";
-                            features.add(new Feature("Dwarven Armor Training", "You have proficiency with light and medium armor.", "Mountain Dwarf", "PHB"));
                             skills.addskill(new Skill("light armor", 1,  "A"));
                             skills.addskill(new Skill("medium armor", 1,  "A"));
 
@@ -460,7 +459,7 @@ public class Main {
                         features.add(new Feature("Darkvision", "Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray.", "Elf", "PHB"));
                         features.add(new Feature("Fey Ancestry", "You have advantage on saving throws against being charmed, and magic can't put you to sleep.", "Elf", "PHB"));
                         features.add(new Feature("Trance", "Elves do not sleep. Instead they meditate deeply, remaining semi-conscious, for 4 hours a day. The Common word for this meditation is \"trance.\" While meditating, you dream after a fashion; such dreams are actually mental exercises that have become reflexive after years of practice. After resting in this way, you gain the same benefit a human would from 8 hours of sleep.", "Elf", "PHB"));
-                        features.add(new Feature("Keen Senses", "You have proficiency in the Perception skill.", "Elf", "PHB"));
+                        skills.addskill(new Skill("perception", 1,  "Sk"));
                         features.add(new Feature("Languages", "You can speak, read, and write Common and Elven.", "Elf", "PHB"));
 
                         uInint = 0;
@@ -471,13 +470,19 @@ public class Main {
                             Int += 1;
                             subrace = "High Elf";
                             features.add(new Feature("Cantrip", "You know one cantrip of your choice from the Wizard spell list. Intelligence is your spellcasting ability for it.", "High Elf", "PHB"));
-                            features.add(new Feature("Elf Weapon Training", "You have proficiency with the longsword, shortsword, shortbow, and longbow.", "High Elf", "PHB"));
+                            skills.addskill(new Skill("longswords", 1,  "mW"));
+                            skills.addskill(new Skill("shortswords", 1,  "mW"));
+                            skills.addskill(new Skill("shortbows", 1,  "sW"));
+                            skills.addskill(new Skill("longbows", 1,  "mW"));
                             features.add(new Feature("Extra Language", "You can read, speak, and write one additional language of your choice.", "High Elf", "PHB"));
 
                         }else if (uInint == 2){
                             Wis += 1;
                             subrace = "Wood Elf";
-                            features.add(new Feature("Elf Weapon Training", "You have proficiency with the longsword, shortsword, shortbow, and longbow.", "Wood Elf", "PHB"));
+                            skills.addskill(new Skill("longswords", 1,  "mW"));
+                            skills.addskill(new Skill("shortswords", 1,  "mW"));
+                            skills.addskill(new Skill("shortbows", 1,  "sW"));
+                            skills.addskill(new Skill("longbows", 1,  "mW"));
                             features.add(new Feature("Fleet of Foot", "Your base walking speed increases to 35 feet.", "Wood Elf", "PHB"));
                             speed = 35;
                             features.add(new Feature("Mask of the Wild", "You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.", "Wood Elf", "PHB"));
@@ -488,7 +493,9 @@ public class Main {
                             features.add(new Feature("Superior Darkvision", "Your darkvision has a range of 120 feet, instead of 60.", "Dark Elf", "PHB"));
                             features.add(new Feature("Sunlight Sensitivity", "You have disadvantage on attack rolls and Wisdom (Perception) checks that rely on sight when you, the target of the attack, or whatever you are trying to perceive is in direct sunlight.", "Dark Elf", "PHB"));
                             features.add(new Feature("Drow Magic", " You know the Dancing Lights cantrip. When you reach 3rd level, you can cast the Faerie Fire spell once with this trait and regain the ability to do so when you finish a long rest. When you reach 5th level, you can cast the Darkness spell once and regain the ability to do so when you finish a long rest. Charisma is your spellcasting ability for these spells.", "Dark Elf", "PHB"));
-                            features.add(new Feature("Drow Weapon Training", "You have proficiency with rapiers, shortswords, and hand crossbows.", "Dark Elf", "PHB"));
+                            skills.addskill(new Skill("rapiers", 1,  "mW"));
+                            skills.addskill(new Skill("shortswords", 1,  "mW"));
+                            skills.addskill(new Skill("hand crossbows", 1,  "mW"));
 
                         }
                     }
