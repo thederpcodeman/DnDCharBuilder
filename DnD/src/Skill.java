80a3b221d09313fe276e11d466f2b3825cc24fa7
connertm24 <connermckeone42@gmail.com>
2024-03-21 08:44:30 -0700

    Proficiency system

--- DnD/src/Skill.java
+++ DnD/src/Skill.java
@@ -4,9 +4,10 @@ public class Skill {
     public int mod; // 1:Str, 2:Dex, 3:Con, 4:Int, 5:Wis, 6:Cha, Anything_else:None
 
     public String type; // A, mW, sW, Sk, T, L
-    public Skill(String call, int lv, int m){
+    public Skill(String call, int lv, int m, String t){
         name = call.toLowerCase();
         level = lv;
         mod = m;
+        type = t;
     }
 }
