38feb1e90929162bcb6ee611f03210ee71a57a3b
connertm24 <connermckeone42@gmail.com>
2024-04-03 09:55:31 -0700

    Fighting Style Class

---
+++ DnD/src/FightingStyle.java
@@ -0,0 +1,8 @@
+public class FightingStyle extends Skill {
+    public String Discription;
+    public FightingStyle(String call, int lv, String Disc){
+        super(call, lv, "Fs");
+        Discription = Disc;
+
+    }
+}
